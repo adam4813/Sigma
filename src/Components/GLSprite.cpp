4fd27b15aaaf57407a6bdba22a78f895c1b996c2
Adam <adam.madram@gmail.com>
2013-08-08 22:01:25 -0500

    GLSprite has an offset now.
    GLSprite has an offset parameter to draw it offset from the entity
    position.
    
    Alos the element buffer index is now stored in GLSprite.

--- src/Components/GLSprite.cpp
+++ src/Components/GLSprite.cpp
@@ -1,5 +1,59 @@
 #include "GLSprite.h"
 
-GLSprite::GLSprite( const int entityID /*= 0*/ ) : IComponent(entityID) {
+GLSprite::GLSprite( const int entityID /*= 0*/ ) : IComponent(entityID), offsetX(0.0f), offsetY(0.0f) {
 
+}
+
+unsigned int GLSprite::VertBuf() const {
+	return this->vertBuf;
+}
+
+void GLSprite::VertBuf(unsigned int vb) {
+	this->vertBuf = vb;
+}
+
+unsigned int GLSprite::ColBuf() const {
+	return this->colBuf;
+}
+
+void GLSprite::ColBuf(unsigned int cb) {
+	this->colBuf = cb;
+}
+
+unsigned int GLSprite::Vao() const {
+	return this->vao;
+}
+
+void GLSprite::Vao(unsigned int v) {
+	this->vao = v;
+}
+
+unsigned int GLSprite::ElemBuf() const {
+	return this->elemBuf;
+}
+
+void GLSprite::ElemBuf(unsigned int eb) {
+	this->elemBuf = eb;
+}
+
+float GLSprite::OffsetX() const {
+	return this->offsetX;
+}
+
+void GLSprite::OffsetX(float x) {
+	if (this->offsetX + x > 1.0f) {
+		this->offsetX = -1.0f;
+	}
+	this->offsetX += x;
+}
+
+float GLSprite::OffsetY() const {
+	return this->offsetY;
+}
+
+void GLSprite::OffsetY(float y) {
+	if (this->offsetY + y > 1.0f) {
+		this->offsetY = -1.0f;
+	}
+	this->offsetY += y;
 }
--- src/Components/GLSprite.h
+++ src/Components/GLSprite.h
@@ -9,14 +9,22 @@ class GLSprite : public IComponent {
 public:
 	GLSprite(const int entityID = 0);
 
-	unsigned int VertBuf() const { return vertBuf; }
-	void VertBuf(unsigned int val) { vertBuf = val; }
-	unsigned int ColBuf() const { return colBuf; }
-	void ColBuf(unsigned int val) { colBuf = val; }
-	unsigned int Vao() const { return vao; }
-	void Vao(unsigned int val) { vao = val; }
+	unsigned int VertBuf() const;
+	void VertBuf(unsigned int val);
+	unsigned int ColBuf() const;
+	void ColBuf(unsigned int val);
+	unsigned int Vao() const;
+	void Vao(unsigned int val);
+	unsigned int ElemBuf() const;
+	void ElemBuf(unsigned int val);
+	void OffsetX(float x);
+	float OffsetX() const;
+	void OffsetY(float y);
+	float OffsetY() const;
 private:
 	unsigned int vertBuf;    // will point to where GL put the buffer
 	unsigned int colBuf;    // will point to where GL put the buffer
+	unsigned int elemBuf;
 	unsigned int vao;
+	float offsetX, offsetY;
 };
