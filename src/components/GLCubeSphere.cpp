88aeb0d2b79765cfb35560dca818c6e7d39c0581
Adam <adam.madram@gmail.com>
2013-09-17 01:39:30 -0500

    Fixed duplicate body for GLCubeSphere::InitializeBuffers()

--- src/components/GLCubeSphere.cpp
+++ src/components/GLCubeSphere.cpp
@@ -225,8 +225,6 @@ void GLCubeSphere::SubDivide(int levels) {
 	}
 }
 
-void GLCubeSphere::InitializeBuffers() { }
-
 void GLCubeSphere::LoadShader() {
 	GLCubeSphere::shader.LoadFromFile(GL_VERTEX_SHADER, "../../shaders/cubesphere.vert");
 	GLCubeSphere::shader.LoadFromFile(GL_FRAGMENT_SHADER, "../../shaders/cubesphere.frag");
