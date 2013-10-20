de86059c85ea9aebd8875a2c6e88577f3f77971a
Meisaka <Meisaka.Yukara@gmail.com>
2013-10-20 16:53:29 -0500

    Fixed typo and converted whitespace

--- src/os/sdl/SDLSys.cpp
+++ src/os/sdl/SDLSys.cpp
@@ -9,67 +9,66 @@ Sigma::event::MouseInputSystem IOpSys::MouseEventSystem;
 //double IOpSys::curDelta;
 
 void* SDLSys::CreateGraphicsWindow(const unsigned int width, const unsigned int height) {
-	std::cout << "Creating Window using SDL..." << std::endl;
+    std::cout << "Creating Window using SDL..." << std::endl;
 
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
 
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
-	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
 
-	this->_Window = SDL_CreateWindow("Sigma Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN );
-	this->_Context = SDL_GL_CreateContext(this->_Window);
+    this->_Window = SDL_CreateWindow("Sigma Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN );
+    this->_Context = SDL_GL_CreateContext(this->_Window);
 
-	GLenum glew_err = glewInit();
-	if (glew_err != GLEW_OK) {
-		fprintf(stderr, "GLEW Error: %s\n", glewGetErrorString(glew_err));
-		return 0;
-	}
+    GLenum glew_err = glewInit();
+    if (glew_err != GLEW_OK) {
+        fprintf(stderr, "GLEW Error: %s\n", glewGetErrorString(glew_err));
+        return 0;
+    }
 
-	this->Fullscreen = false;
-	glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LESS);
+    this->Fullscreen = false;
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
 
-	// Set the relative mouse state (hide mouse)
-	/*
-	if(SDL_SetRelativeMouseMode(SDL_FALSE) < 0){
+    // Set the relative mouse state (hide mouse)
+    /*
+    if(SDL_SetRelativeMouseMode(SDL_FALSE) < 0){
         std::cerr << "ERROR: SDL could not grab mouse\n" << SDL_GetError() << std::endl;
-	} */
-	// relative mode was breaking things, just hide mouse.
-	if(SDL_ShowCursor(SDL_DISABLE) < 0) {
+    } */
+    // relative mode was breaking things, just hide mouse.
+    if(SDL_ShowCursor(SDL_DISABLE) < 0) {
         std::cerr << "ERROR: SDL could not hide mouse\n" << SDL_GetError() << std::endl;
-	}
+    }
 
-	return &this->_Context;
+    return &this->_Context;
 }
 
 bool SDLSys::MessageLoop() {
 
-	SDL_Event event;
+    SDL_Event event;
 
-	// Keyboard varables
-	SDL_Keycode key;
+    // Keyboard varables
+    SDL_Keycode key;
 
-	// Mouse variables
-	float width, height, dx=0.0f, dy=0.0f;
-	int movx, movy; // test
-	float mouse_sensitivity = 100.0f;
-	float dampener = 2.0f;
-	bool mouse_moved = false; // if no mouse motion, we will explicitly clear it
+    // Mouse variables
+    float width, height, dx=0.0f, dy=0.0f;
+    float mouse_sensitivity = 100.0f;
+    float dampener = 2.0f;
+    bool mouse_moved = false; // if no mouse motion, we will explicitly clear it
 
-	width = (float)GetWindowWidth();
-	height = (float)GetWindowHeight();
+    width = (float)GetWindowWidth();
+    height = (float)GetWindowHeight();
 
-	this->_KeyReleased.clear();
+    this->_KeyReleased.clear();
 
-	while(SDL_PollEvent(&event)) {
-		switch(event.type) {
-		case SDL_QUIT:
-			SDL_ShowCursor(SDL_ENABLE); // show mouse, since we are exiting
-			return false;
-		case SDL_KEYDOWN:
+    while(SDL_PollEvent(&event)) {
+        switch(event.type) {
+        case SDL_QUIT:
+            SDL_ShowCursor(SDL_ENABLE); // show mouse, since we are exiting
+            return false;
+        case SDL_KEYDOWN:
             // hack to do case insensitive lookup
             key = event.key.keysym.sym;
             if (key > 96 && key < 123) {
@@ -78,44 +77,44 @@ bool SDLSys::MessageLoop() {
             this->_KeyStates[key] = true;
             KeyboardEventSystem.KeyDown(key);
             break;
-		case SDL_KEYUP:
-			// hack to do case insensitive lookup
-			key = event.key.keysym.sym;
-			if (key > 96 && key < 123) {
-				key -= 32;
-			}
-			this->_KeyStates[key] = false;
-			this->_KeyReleased[key] = true;
-			KeyboardEventSystem.KeyUp(key);
-			break;
-		case SDL_MOUSEMOTION:
-		    mouse_moved = true;
+        case SDL_KEYUP:
+            // hack to do case insensitive lookup
+            key = event.key.keysym.sym;
+            if (key > 96 && key < 123) {
+                key -= 32;
+            }
+            this->_KeyStates[key] = false;
+            this->_KeyReleased[key] = true;
+            KeyboardEventSystem.KeyUp(key);
+            break;
+        case SDL_MOUSEMOTION:
+            mouse_moved = true;
             /*printf("Mouse moved by %d,%d to (%d,%d)\n",
                     event.motion.xrel, event.motion.yrel,
                     event.motion.x, event.motion.y);// */
-			if(!(event.montion.xrel || event.motion.yrel)) {
-				return true; // Exit if no motion to avoid spamming mouse events
-			}
-			// Motion is in pixels, so normalize to -1.0 to 1.0 range
-			// Relative mouse motion is restricted, so scale the movement
-			// and dampen small movements
-			if(abs(event.motion.xrel) < dampener) {
-				dx = 0.0f;
-			} else {
-				dx = mouse_sensitivity * (event.motion.xrel / width);
-			}
+            if(!(event.motion.xrel || event.motion.yrel)) {
+                return true; // Exit if no motion to avoid spamming mouse events
+            }
+            // Motion is in pixels, so normalize to -1.0 to 1.0 range
+            // Relative mouse motion is restricted, so scale the movement
+            // and dampen small movements
+            if(abs(event.motion.xrel) < dampener) {
+                dx = 0.0f;
+            } else {
+                dx = mouse_sensitivity * (event.motion.xrel / width);
+            }
 
-			if(abs(event.motion.yrel) < dampener) {
-				dy = 0.0f;
-			} else {
-				dy = mouse_sensitivity * (event.motion.yrel / height);
-			}
+            if(abs(event.motion.yrel) < dampener) {
+                dy = 0.0f;
+            } else {
+                dy = mouse_sensitivity * (event.motion.yrel / height);
+            }
 
-			// Dispatch the event
-			MouseEventSystem.MouseMove(dx,dy);
+            // Dispatch the event
+            MouseEventSystem.MouseMove(dx, dy);
 
-			// if mouse is hidden/grabbed and moved, recenter it so it doesn't appear offscreen
-		        SDL_WarpMouseInWindow(this->_Window, (width / 2.0f), (height / 2.0f));
+            // if mouse is hidden/grabbed and moved, recenter it so it doesn't appear offscreen
+                SDL_WarpMouseInWindow(this->_Window, (width / 2.0f), (height / 2.0f));
             break;
         case SDL_MOUSEBUTTONDOWN:
             printf("Mouse button %d pressed at (%d,%d)\n",
@@ -123,85 +122,85 @@ bool SDLSys::MessageLoop() {
             printf("Mouse button pressed \n");
 
             break;
-		}
-	}
+        }
+    }
 
-	// explicitly clear mouse motion if no signal was received
-	// removed to allow immediate mode
-	/*
-	if(!mouse_moved){
+    // explicitly clear mouse motion if no signal was received
+    // removed to allow immediate mode
+    /*
+    if(!mouse_moved){
         MouseEventSystem.MouseMove(0.f, 0.f);
-	}*/
+    }*/
 
-	return true;
+    return true;
 }
 
 bool SDLSys::SetupTimer() {
-	this->_LastTime = SDL_GetTicks();
-	return true;
+    this->_LastTime = SDL_GetTicks();
+    return true;
 }
 
 void SDLSys::ToggleFullscreen() {
-	if(!this->Fullscreen) {
-		SDL_SetWindowFullscreen(this->_Window,SDL_WINDOW_FULLSCREEN_DESKTOP);
-	}
-	else {
-		SDL_SetWindowFullscreen(this->_Window,0);
-	}
-	this->Fullscreen=!this->Fullscreen;
+    if(!this->Fullscreen) {
+        SDL_SetWindowFullscreen(this->_Window,SDL_WINDOW_FULLSCREEN_DESKTOP);
+    }
+    else {
+        SDL_SetWindowFullscreen(this->_Window,0);
+    }
+    this->Fullscreen=!this->Fullscreen;
 }
 
 unsigned int SDLSys::GetWindowHeight() {
-	int width,height;
-	SDL_GetWindowSize(this->_Window,&width,&height);
-	return height;
+    int width,height;
+    SDL_GetWindowSize(this->_Window,&width,&height);
+    return height;
 }
 
 unsigned int SDLSys::GetWindowWidth() {
-	int width,height;
-	SDL_GetWindowSize(this->_Window,&width,&height);
-	return width;
+    int width,height;
+    SDL_GetWindowSize(this->_Window,&width,&height);
+    return width;
 }
 
 double SDLSys::GetDeltaTime() {
-	int currentTime = SDL_GetTicks();
-	double delta = static_cast<double>(currentTime - this->_LastTime);
-	this->_LastTime = currentTime;
-	return delta;
+    int currentTime = SDL_GetTicks();
+    double delta = static_cast<double>(currentTime - this->_LastTime);
+    this->_LastTime = currentTime;
+    return delta;
 }
 
 bool SDLSys::KeyDown(int key, bool focused) {
-	// Check if the focused bool is true, and that
-	// the window we are using has keyboard focus
-	if(focused && this->_Window == SDL_GetKeyboardFocus()) {
-		return this->_KeyStates[key];
-	}
-	else {
-		return false;
-	}
+    // Check if the focused bool is true, and that
+    // the window we are using has keyboard focus
+    if(focused && this->_Window == SDL_GetKeyboardFocus()) {
+        return this->_KeyStates[key];
+    }
+    else {
+        return false;
+    }
 }
 bool SDLSys::KeyUp(int key, bool focused /*= false */) {
-	// Check if the focused bool is true, and that
-	// the window we are using has keyboard focus
-	if(focused && this->_Window == SDL_GetKeyboardFocus()) {
-		return !this->_KeyStates[key];
-	}
-	else {
-		return false;
-	}
+    // Check if the focused bool is true, and that
+    // the window we are using has keyboard focus
+    if(focused && this->_Window == SDL_GetKeyboardFocus()) {
+        return !this->_KeyStates[key];
+    }
+    else {
+        return false;
+    }
 }
 
 
 bool SDLSys::KeyReleased(int key, bool focused /*= false */) {
-	if(focused && this->_Window == SDL_GetKeyboardFocus()) {
-		return this->_KeyReleased[key];
-	}
-	else {
-		return false;
-	}
+    if(focused && this->_Window == SDL_GetKeyboardFocus()) {
+        return this->_KeyReleased[key];
+    }
+    else {
+        return false;
+    }
 }
 
 
 void SDLSys::Present() {
-	SDL_GL_SwapWindow(this->_Window);
+    SDL_GL_SwapWindow(this->_Window);
 }
