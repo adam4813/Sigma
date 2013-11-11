f462aff451f83f664e403dbc5c8fb50fd4539f4f
Adam Martin <adam.madram@gmail.com>
2013-11-11 02:16:13 -0600

    Cleaned up main and added WebGUISys and GUIControllers
    The controllers are hard coded to 3 GUI components entity ID 100, 101, and 102.

--- src/main.cpp
+++ src/main.cpp
@@ -7,30 +7,25 @@
 #include "controllers/GUIController.h"
 #include "controllers/FPSCamera.h"
 #include "components/BulletMover.h"
+#include "components/GLScreenQuad.h"
 #include "SCParser.h"
+#include "systems/WebGUISystem.h"
 
 #if defined OS_Win32
 #include "os/win32/win32.h"
 #elif defined OS_SDL
 #include "os/sdl/SDLSys.h"
 #endif
-<<<<<<< HEAD
-=======
-#include "components/GLSprite.h"
-
-//GUI url and some random things to test javascript bindings
-#define URL     "http://0x10co.de/btc1h"
-#define NAME    "Sigma"
-#define VERSION 1
->>>>>>> Added the GLTexture resource
 
 int main(int argCount, char **argValues) {
 	Sigma::OpenGLSystem glsys;
 	Sigma::BulletPhysics bphys;
+	Sigma::WebGUISystem webguisys;
 
 	Sigma::FactorySystem& factory = Sigma::FactorySystem::getInstance();
 	factory.register_Factory(glsys);
 	factory.register_Factory(bphys);
+	factory.register_Factory(webguisys);
 
 	IOpSys* os = nullptr;
 
@@ -57,27 +52,15 @@ int main(int argCount, char **argValues) {
 		std::cerr << "Error starting OpenGL!" << std::endl;
 		delete os;
 		return -1;
-	}
-	else {
+	} else {
 		std::cout << "OpenGL version: " << version[0] << "." << version[1] << std::endl;
 	}
 
 	bphys.Start();
 
-<<<<<<< HEAD
-=======
-	//For web gui
-	std::vector<Property> props;
 	webguisys.Start();
-	webguisys.createWebView("WebGUIComponent", 21, props, URL);
-	props.push_back(Property("textureFilename", std::string("gui")));
-	glsys.createGLSprite(0, props);
-	Sigma::event::handler::GUIController guicon;
-	guicon.SetGUI(webguisys.getComponent(21, WebGUIComponent::getStaticComponentTypeName()));
-	IOpSys::KeyboardEventSystem.Register(&guicon);
-	IOpSys::MouseEventSystem.Register(&guicon);
+	webguisys.SetWindowSize(os->GetWindowWidth(), os->GetWindowHeight());
 
->>>>>>> Added the GLTexture resource
 	// Parse the scene file to retrieve entities
 	Sigma::parser::SCParser parser;
 
@@ -92,7 +75,6 @@ int main(int argCount, char **argValues) {
 	for (unsigned int i = 0; i < parser.EntityCount(); ++i) {
 		Sigma::parser::Entity* e = parser.GetEntity(i);
 		for (auto itr = e->components.begin(); itr != e->components.end(); ++itr) {
-
 			// Currently, physicsmover components must come after gl* components
 			if((*itr).type == "PhysicsMover") {
 				GLTransform *transform = glsys.GetTransformFor(e->id);
@@ -105,9 +87,7 @@ int main(int argCount, char **argValues) {
 				}
 			}
 
-            factory.create(
-                        itr->type,e->id,
-                        const_cast<std::vector<Property>&>(itr->properties));
+            factory.create(itr->type,e->id, const_cast<std::vector<Property>&>(itr->properties));
 		}
 	}
 
@@ -135,21 +115,34 @@ int main(int argCount, char **argValues) {
 	// Link the graphics view to the physics system's view mover
 	Sigma::BulletMover* mover = bphys.getViewMover();
 
-
 	// Create the controller
 	// Perhaps a little awkward currently, should create a generic
 	// controller class ancestor
-	if (glsys.GetViewMode() == "FPSCamera") {
+	if(glsys.GetViewMode() == "FPSCamera") {
 	    using Sigma::event::handler::FPSCamera;
         FPSCamera* theCamera = static_cast<FPSCamera*>(glsys.GetView());
 		IOpSys::KeyboardEventSystem.Register(theCamera);
 		IOpSys::MouseEventSystem.Register(theCamera);
 		theCamera->SetMover(mover);
-	}
-	else if (glsys.GetViewMode() == "GLSixDOFView") {
+	} else if (glsys.GetViewMode() == "GLSixDOFView") {
 		Sigma::event::handler::GLSixDOFViewController cameraController(glsys.GetView(), mover);
 		IOpSys::KeyboardEventSystem.Register(&cameraController);
 	}
+
+	Sigma::event::handler::GUIController guicon;
+	guicon.SetGUI(webguisys.getComponent(100, Sigma::WebGUIView::getStaticComponentTypeName()));
+	IOpSys::KeyboardEventSystem.Register(&guicon);
+	IOpSys::MouseEventSystem.Register(&guicon);
+
+	Sigma::event::handler::GUIController guicon2;
+	guicon2.SetGUI(webguisys.getComponent(101, Sigma::WebGUIView::getStaticComponentTypeName()));
+	IOpSys::KeyboardEventSystem.Register(&guicon2);
+	IOpSys::MouseEventSystem.Register(&guicon2);
+
+	Sigma::event::handler::GUIController guicon3;
+	guicon3.SetGUI(webguisys.getComponent(102, Sigma::WebGUIView::getStaticComponentTypeName()));
+	IOpSys::KeyboardEventSystem.Register(&guicon3);
+	IOpSys::MouseEventSystem.Register(&guicon3);
 
 	// Setup the timer
 	os->SetupTimer();
@@ -159,7 +152,6 @@ int main(int argCount, char **argValues) {
 	bool isWireframe=false;
 
 	// Load a font
-
 	while (os->MessageLoop()) {
 
 		// Get time in ms, store it in seconds too
@@ -171,8 +163,7 @@ int main(int argCount, char **argValues) {
 			if (!isWireframe) {
 				glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
 				isWireframe = true;
-			}
-			else {
+			} else {
 				glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
 				isWireframe = false;
 			}
