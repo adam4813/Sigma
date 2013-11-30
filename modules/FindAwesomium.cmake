5d47e5f0a145bbe8b957ef9d9b657f49dd4b2629
wrongu <lange.richard.d@gmail.com>
2013-11-30 18:09:43 -0500

    first round of fixes
    FindAwesomium and AWESOMIUM_LIBS added/fixed
    OS.h include wglew.h is now guarded from *nix

---
+++ modules/FindAwesomium.cmake
@@ -0,0 +1,34 @@
+include (FindPackageHandleStandardArgs)
+
+find_path(AWESOMIUM_INCLUDE_DIR Awesomium/WebCore.h
+	PATH_SUFFIXES include
+	PATHS
+	${AWESOMIUM_ROOT}
+	$ENV{AWESOMIUM_ROOT}
+	~/Library/Frameworks
+	/Library/Frameworks
+	/usr/local/
+	/usr/
+	/sw          # Fink
+	/opt/local/  # DarwinPorts
+	/opt/csw/    # Blastwave
+	/opt/)
+	
+find_library(AWESOMIUM_LIBRARY 
+	NAMES awesomium
+	PATH_SUFFIXES lib64 lib build/lib 
+	PATHS
+	${AWESOMIUM_ROOT}
+	$ENV{AWESOMIUM_ROOT}
+	~/Library/Frameworks
+	/Library/Frameworks
+	/usr/local/
+	/usr/
+	/sw          # Fink
+	/opt/local/  # DarwinPorts
+	/opt/csw/    # Blastwave
+	/opt/)
+
+find_package_handle_standard_args (AWESOMIUM DEFAULT_MSG AWESOMIUM_LIBRARY AWESOMIUM_INCLUDE_DIR)
+mark_as_advanced (AWESOMIUM_INCLUDE_DIR AWESOMIUM_LIBRARY)
+
