a605a88b746da58ecc4e01caa3386a183eef94c0
catageek <catageek@free.fr>
2013-12-08 15:21:56 +0100

    Adding an array pool used by WorldPosition class when transforming model positions to player positions.
    The pool gives fixed-size arrays until reset to a new size value.
    
    Arrays are given as shared_ptr.

---
+++ include/components/ArrayPool.hpp
@@ -0,0 +1,59 @@
+#ifndef ARRAYPOOL_HPP_INCLUDED
+#define ARRAYPOOL_HPP_INCLUDED
+
+#include <stdexcept>
+#include <forward_list>
+#include <memory>
+#include <cassert>
+#include "SIMD.hpp"
+
+using std::shared_ptr;
+using std::forward_list;
+
+namespace Sigma {
+    template<class T>
+    class ArrayPool {
+    public:
+        ArrayPool() {};
+
+        virtual ~ArrayPool() {};
+
+        const shared_ptr<T> GetArray(size_t length) {
+            assert(length == length);
+            for (auto& p : pool) {
+                if (p.unique()) {
+                    return p;
+                }
+            }
+            return AllocateArrays(length);
+        }
+
+        void IncreaseLength() {
+            length++;
+            pool.clear();
+        }
+
+        void DecreaseLength() {
+            if (length != 0) {
+                length--;
+                pool.clear();
+            }
+        }
+
+    private:
+        const shared_ptr<T> AllocateArrays(size_t length) {
+            auto ptr = AlignedMalloc<T>(length * sizeof(T));
+            if (ptr == NULL) {
+                throw std::runtime_error("Error: unable to allocate memory");
+            }
+            auto ret = shared_ptr<T>(ptr, AlignedPointerDelete<T>());
+            pool.push_front(ret);
+            return ret;
+        }
+
+        size_t length = 0;
+        forward_list<shared_ptr<T>> pool;
+    };
+}
+
+#endif // ARRAYPOOL_HPP_INCLUDED
