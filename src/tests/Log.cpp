2eeceeee5b3e4f15a1d39c9a846f0b17fee62431
Luis Panadero Guardeño <luis.panadero@gmail.com>
2014-01-13 17:09:04 +0100

    Added a Logger to Sigma, that can outputs to std::cout or any other output stream. Also there a little addition in CMakeFileLists.txt to allow to generate a executable for each one .cpp file in src/tests/

---
+++ src/tests/Log.cpp
@@ -0,0 +1,38 @@
+#include "Log.h"
+#include <fstream>
+
+int main () {
+	{
+		// Example setting the output to a file
+		std::ofstream f("log.txt", std::ios::app);
+		Log::Init(f);
+		
+		LOG_DEBUG << "Hello world!";
+		LOG				<< "Hello world!";
+		LOG_WARN	<< "Hello world!";
+		LOG_ERROR << "Hello world!";
+
+		Log::Level(Log::LogLevel::WARN);	
+		LOG_DEBUG << "Hello world! 2";
+		LOG				<< "Hello world! 2";
+		LOG_WARN	<< "Hello world! 2";
+		LOG_ERROR << "Hello world! 2";
+	
+	} // RAII closes the file here
+
+	// Default behaviur. outputs to std::clog (std::cerr)
+	Log::Init();
+	
+	LOG_DEBUG << "Hi mom!";
+	LOG				<< "Yo should know that";
+	LOG_WARN	<< "Danger Will Robinson";
+	LOG_ERROR << "404!";
+
+	Log::Level(Log::LogLevel::WARN);	
+	LOG_DEBUG << "You should see this";
+	LOG				<< "Too verbose...";
+	LOG_WARN	<< "Alarm!";
+	LOG_ERROR << "Dave, don't do that";
+
+	return 0;
+}
